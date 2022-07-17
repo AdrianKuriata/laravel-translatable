50c6137ab3b3c679dedc958dc0de77b852127eb8
Adrian Kuriata <adrian.kuriata@znd.pl>
2022-07-17 21:22:31 +0200

    Secondary initial commit

---
+++ src/LaravelTranslatableServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Devsite\LaravelTranslatable;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelTranslatableServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
